2c70016931650f57bbbf29c6939af97aa8057104
Helena Strada <h.strada@hotmail.com>
2018-05-16 12:11:05 -0300

    conteúdo revisão

---
+++ senai-android-revisao/jogos/src/main/java/sp/senai/br/jogos/JogosApplication.java
@@ -0,0 +1,12 @@
+package sp.senai.br.jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogosApplication.class, args);
+	}
+}
